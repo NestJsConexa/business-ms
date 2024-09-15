dde21ec5923f898ae5f4c5f8da7a31647795b9d0
candelaria_sobol <csobol@mobydigital.com>
2024-09-15 12:23:50 -0300

    Configure database connection with Mongoose and environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import Joi from 'joi';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required()
+      })
+    }),
+    DatabaseModule
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
